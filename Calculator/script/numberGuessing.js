fae4ea53fd654735f7148e861a39d2cca22625f5
Calvin Then <39335528+calvinthen@users.noreply.github.com>
2025-03-08 22:02:18 +0700

    finish nunber guessing game with JS

---
+++ Calculator/script/numberGuessing.js
@@ -0,0 +1,44 @@
+// let info = alert("Guess between number 1 - 100");
+// let answer = prompt('Type here');
+
+// console.log(info , answer);
+
+// console.log("User Pick number  : " + answer);
+
+const maxNum = 100;
+const minNum = 1;
+const answer = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
+
+let attempts = 0;
+let guess;
+let running = true;
+
+while(running)
+{
+    guess = window.prompt(`Guess a number between ${minNum} - ${maxNum}`);
+    guess = Number(guess);
+
+    if(isNaN(guess)){
+        window.alert("Please enter a number");
+    }
+    else if(guess < minNum || guess > maxNum)
+    {
+        window.alert("Please enter a valid number");
+    }
+    else{
+        attempts++;
+        if(guess < answer)
+        {
+            window.alert("The answer is higher than you input ! try again");
+        }
+        else if(guess > answer)
+        {
+            window.alert("The answer is lower than you input ! try again");
+        }
+        else
+        {
+            window.alert(`Correct the answer is ${answer}. it took you ${attempts} times to find it !`);
+            running = false;
+        }
+    }
+}
