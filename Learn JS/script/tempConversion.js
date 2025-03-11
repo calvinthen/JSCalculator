14d6ca4c51efcbe61e1d594fffceb4f739c6d6a2
Calvin Then <39335528+calvinthen@users.noreply.github.com>
2025-03-11 23:05:45 +0700

    preparing JS variable

--- Learn JS/script/tempConversion.js
+++ Learn JS/script/tempConversion.js
@@ -1,3 +1,9 @@
+const textBox = document.getElementById("textBox");
+const toFahrenheit = document.getElementById("toFahrenheit");
+const toCelcius = document.getElementById("toCelcius");
+const result = document.getElementById("result");
+let temp;
+
 function convert(){
     
 }
