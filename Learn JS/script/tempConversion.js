49cbac944c605a7283059ebd8c61cf9db03c811c
Calvin Then <39335528+calvinthen@users.noreply.github.com>
2025-03-12 14:37:01 +0700

    finished js formula for celcius and fahreint conversion

--- Learn JS/script/tempConversion.js
+++ Learn JS/script/tempConversion.js
@@ -5,5 +5,22 @@ const result = document.getElementById("result");
 let temp;
 
 function convert(){
+
+    if(toFahrenheit.checked)
+    {
+        temp = Number(textBox.value);
+        temp = temp * 9 / 5 + 32;
+        result.textContent = textBox.value + " Celcius is " + temp.toFixed(1) + " Fahrenheit";
+    }
+    else if(toCelcius.checked)
+    {
+        temp = Number(textBox.value);
+        temp = (temp - 32) * (5/9);
+        result.textContent = textBox.value + " Fahrenheit is " + temp.toFixed(1) + " Celcius";
+
+    }
+    else{
+        result.textContent = "Please select a Unit";
+    }
     
 }
