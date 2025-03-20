5592d92b417b3bb7edbbf244b903da82a529b914
Calvin Then <39335528+calvinthen@users.noreply.github.com>
2025-03-20 17:30:16 +0700

    create ui for generate and debugging logic on js

--- Learn JS/script/passwordGenerator.js
+++ Learn JS/script/passwordGenerator.js
@@ -5,7 +5,15 @@ function generatePassword(length, includeLowerCase, includeUpperCase, includeNum
     const numbers = "1234567890";
     const symbols = "`!@#$%^&*()_-+=[]{}|";
 
-    
+    let allowedChars = "";
+    let password = "";
+
+    allowedChars += includeLowerCase ? lowerCaseChars : "";
+    allowedChars += includeUpperCase ? upperCaseChars : "";
+    allowedChars += includeNumbers ? numbers : "";
+    allowedChars += includeSymbols ? symbols : "";
+
+    console.log(allowedChars);
     return '';
 }
 
@@ -15,7 +23,23 @@ const includeUpperCase = true;
 const includeNumbers = true;
 const includeSymbols = true;
 
+//Checkbox
+const lowerCaseBox = document.getElementById("lowerCaseBox");
+
 const password =  generatePassword(passwordLength, includeLowerCase, includeUpperCase, 
     includeNumbers, includeSymbols)
 
 console.log(`Generated password: ${password}`);
+
+function includedPasswordCheckBox()
+{
+    if(lowerCaseBox.checked)
+    {
+        console.log("Lower Case Included");    
+    }   
+    else
+    {
+        console.log("Lower Case not Included");
+    }
+    console.log("Jalan");
+}
